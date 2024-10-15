45ebdf2ed1ce8f78a6789944706ca12c591a63ed
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-10-15 17:21:50 +0600

    register done

--- accounts/views.py
+++ accounts/views.py
@@ -10,13 +10,11 @@ class UserRegistrationView(FormView):
     success_url = reverse_lazy('register')
 
     def form_valid(self, form):
+        print(form.cleaned_data)
         user = form.save()
-        login(user)
+        login(self.request, user)
+        print(user)
         return super().form_valid(form)
-
-
-
-
 
 
 
