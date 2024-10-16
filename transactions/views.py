d2c836d44e9fbe1bf51adf46c2aef36ed5468eff
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-10-16 13:05:26 +0600

    added withdraw and loan req view

--- transactions/views.py
+++ transactions/views.py
@@ -5,6 +5,7 @@ from .models import Transection
 from .forms import DepositForm, WithdrawForm, LoanRequestForm
 from .constants import DEPOSIT, WITHDRAWAL, LOAN, LOAN_PAID
 from django.contrib import messages
+from django.http import HttpResponse
 # Create your views here.
 class TransectionCreateMixin(LoginRequiredMixin, CreateView):
     template_name = ''
@@ -25,9 +26,9 @@ class TransectionCreateMixin(LoginRequiredMixin, CreateView):
             'title' : self.title
         })
 
-class DepositeMoneyView(TransectionCreateMixin):
+class DepositMoneyView(TransectionCreateMixin):
     form_class = DepositForm
-    title = 'Deposite'
+    title = 'Deposit'
 
     def get_initial(self):
         initial = {'transaction_type' : DEPOSIT}
@@ -42,5 +43,40 @@ class DepositeMoneyView(TransectionCreateMixin):
         )
 
         messages.success(self.request, f'{amount} BDT was deposited to you account successfully')
+        return super().form_valid(form)
+
+class WithdrawMoneyView(TransectionCreateMixin):
+    form_class = WithdrawForm
+    title = 'Withdraw Money'
+
+    def get_initial(self):
+        initial = {'transaction_type' : WITHDRAWAL}
+        return initial
+    
+    def form_valid(self, form):
+        amount = form.cleaned_data.get('amount')
+        account = self.request.user.account
+        account.balance -= amount
+        account.save(
+            update_fields = ['balance']
+        )
+
+        messages.success(self.request, f'Successfully withdrawn {amount} BDT from you account successfully')
+        return super().form_valid(form)
+    
+class LoanMoneyView(TransectionCreateMixin):
+    form_class = LoanRequestForm
+    title = 'Request for loan'
+
+    def get_initial(self):
+        initial = {'transaction_type' : LOAN}
+        return initial
+    
+    def form_valid(self, form):
+        amount = form.cleaned_data.get('amount')
+        current_loan_count = Transection.objects.filter(account = self.request.user.account, transaction_type = 3, loan_approve = True).count()
+        if current_loan_count >= 2:
+            return HttpResponse("You have crossed you loan limit")
+        messages.success(self.request, f'loan request for {amount} BDT has been successfully sent to admin')
         return super().form_valid(form)
     
