dc52f9332129ab962f097dfd8eada344fb4fd00f
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-10-16 11:51:44 +0600

    added TransectionCreateMixin

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Transection
 # Create your views here.
+class TransectionCreateMixin(LoginRequiredMixin, CreateView):
+    template_name = ''
+    model = Transection
+    title = ''
+    success_url = ''
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({
+            'account' : self.request.user.account,
+        })
+        return kwargs
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'title' : self.title
+        })
