a38363982faee18266b1c8716bd26e4fd768282b
joyantgupta <joyantsheikharguptajoy@gmail.com>
2024-10-27 17:50:24 +0600

    database added

--- coder_bank/settings.py
+++ coder_bank/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 import environ
+import dj_database_url
 env = environ.Env()
 environ.Env.read_env()
 
@@ -89,15 +90,25 @@ WSGI_APPLICATION = 'coder_bank.wsgi.application'
 # }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
+#     }
+# }
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://coderbank_user:FiwQBchNRku0epNJ2xAqbQCrCWqf2Bv0@dpg-csf2g4btq21c738ikpk0-a.oregon-postgres.render.com/coderbank',
+    )
 }
 
 # Password validation
